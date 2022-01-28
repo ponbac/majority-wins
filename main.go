6670b6c0ac1fdc2fd461487829d1fe12422752f8
ponbac <ponbac@student.chalmers.se>
2022-01-28 16:27:50 +0100

    now prints port fetched from env

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/ponbac/majority-wins/game"
 )
 
 func main() {
-	var addr = flag.String("addr", ":8080", "http service address")
-	flag.Parse()
+	port := os.Getenv("PORT")
+	fmt.Println("Fetched from env: ", port)
+	if port == "" {
+		port = "8080"
+	}
 	room := game.NewRoom()
-	room.Questions = []game.Question{{Type: "Music", Description: "1 or 2, little human?", Answers: make(map[*game.Player]int)}, 
+	room.Questions = []game.Question{{Type: "Music", Description: "1 or 2, little human?", Answers: make(map[*game.Player]int)},
 		{Type: "Trivia", Description: "Is KalleK strong 1=YES, 2=NO?", Answers: make(map[*game.Player]int)}}
 	go room.Run()
 	go room.StartGame()
@@ -20,8 +24,8 @@ func main() {
 		game.ServeWs(room, w, r)
 	})
 
-	log.Println("Starting server on http://localhost" + *addr)
-	err := http.ListenAndServe(*addr, nil)
+	log.Println("Starting server on http://localhost" + ":" + port)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
