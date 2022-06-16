85245dba495290587b683a05fd5f2fb565868097
Pontus Backman <ponbac@student.chalmers.se>
2022-06-16 19:23:27 +0200

    now no longer sends queued messages

--- game/player.go
+++ game/player.go
@@ -132,11 +132,11 @@ func (p *Player) writePump() {
 			w.Write(message)
 
 			// Add queued chat messages to the current websocket message.
-			n := len(p.send)
-			for i := 0; i < n; i++ {
-				w.Write(newline)
-				w.Write(<-p.send)
-			}
+			// n := len(p.send)
+			// for i := 0; i < n; i++ {
+			// 	w.Write(newline)
+			// 	w.Write(<-p.send)
+			// }
 
 			if err := w.Close(); err != nil {
 				return
